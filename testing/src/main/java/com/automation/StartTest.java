ea8f12975cb35ee5edd8a722cb5dcb560413bf3b
nirustanite <nirustanite@gmail.com>
2019-01-12 01:46:49 +0100

    extended the code till payment successful

--- testing/src/main/java/com/automation/StartTest.java
+++ testing/src/main/java/com/automation/StartTest.java
@@ -2,6 +2,8 @@ package com.automation;
 import com.automation.model.*;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 
@@ -19,7 +21,17 @@ public class StartTest extends Common{
         Reporter.log("Driver is loaded");
         
         driver.get(URL);
-        this.TakeScreenshot(driver, "screenshots\\test.png");
+        this.TakeScreenshot(driver, "screenshots1\\test.png");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1000)");
+        driver.findElement(By.xpath("(//*[@name='paymentProductId']/div)[18]")).click();
+        this.TakeScreenshot(driver, "screenshots\\test1.png");
+        driver.findElement(By.xpath("//select[@name='issuerId']/option[11]")).click();
+        driver.findElement(By.id("primaryButton")).click();
+        Common.wait(30);
+        driver.findElement(By.name("button.edit")).click();
+        driver.findElement(By.xpath("//*[@class='paymentoptions']/p"));
+        
     }
   
 	@AfterClass
